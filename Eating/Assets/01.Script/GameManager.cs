b49914bfaa75476f3863ac8bd96a8f07f984ed56
김병현 <>
2022-08-23 21:22:52 +0900

    eatt
    yummy

--- Eating/Assets/01.Script/GameManager.cs
+++ Eating/Assets/01.Script/GameManager.cs
@@ -2,24 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PixelState
+{
+    none,
+    fill,
+    boundary,
+    moveLine
+}
+
 public class GameManager : MonoSingleTon<GameManager>
 {
     [SerializeField] private SpriteRenderer mainSprite;
 
     Vector2 indexPer;
     Vector2 worldPer;
+    Vector2Int textureSize;
+
+    PixelState[,] pixelStates;
+
+    public Texture2D decureTexture;
+    public Texture2D cureTexture;
 
     protected override void Awake()
     {
+        Texture2D mainTexture = Instantiate(decureTexture);
+        mainSprite.sprite = Sprite.Create(mainTexture, new Rect(0, 0, mainTexture.width, mainTexture.height), Vector2.one * 0.5f, mainSprite.sprite.pixelsPerUnit);
         base.Awake();
+        textureSize = new Vector2Int(mainSprite.sprite.texture.width, mainSprite.sprite.texture.height);
+        pixelStates = new PixelState[mainSprite.sprite.texture.width, mainSprite.sprite.texture.height];
         indexPer.x = 1920f / mainSprite.sprite.texture.width;
         indexPer.y = 1080f / mainSprite.sprite.texture.height;
         worldPer.x = mainSprite.sprite.texture.width / 1920;
         worldPer.y = mainSprite.sprite.texture.height / 1080;
     }
 
+    private void FixedUpdate()
+    {
+        mainSprite.sprite.texture.Apply();
+    }
+
     public Vector3 IndexToPos(float x, float y)
     {
        return Camera.main.ScreenToWorldPoint(new Vector3(x * indexPer.x, y * indexPer.y, 10));
+    }
+
+    public Vector2Int ClampedIndex(Vector2Int index)
+    {
+        if (index.x >= textureSize.x)
+            index.x = textureSize.x-1;
+        else if (index.x < 0)
+            index.x = 0;
+
+        if (index.y >= textureSize.y)
+            index.y = textureSize.y-1;
+        else if (index.y < 0)
+            index.y = 0;
+        return index;
+    }
+
+    public void SetPixel(Vector2Int vec, PixelState state)
+    {
+        mainSprite.sprite.texture.SetPixel(vec.x, vec.y, Color.red);
+        pixelStates[vec.x, vec.y] = state;
+    }
+
+    public PixelState GetPixel(Vector2Int vec)
+    {
+        return pixelStates[vec.x, vec.y];
     }
 }
--- Eating/Assets/01.Script/PlayerMoveMent.cs
+++ Eating/Assets/01.Script/PlayerMoveMent.cs
@@ -2,37 +2,131 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveMent : MonoBehaviour
+public class PlayerMoveMent : MonoSingleTon<PlayerMoveMent>
 {
-    KeyCode[] keyArr = new KeyCode[4] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
-    Vector2Int[] dir = new Vector2Int[4] { Vector2Int.up, Vector2Int.left, Vector2Int.down, Vector2Int.right };
-    
+    //KeyCode[] moveKey = new KeyCode[4] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+    //Vector2Int[] dirVec = new Vector2Int[4] { Vector2Int.up, Vector2Int.left, Vector2Int.down, Vector2Int.right };
+    //List<Vector2Int> inputDirList = new();
+
+    //public Vector2Int playerPos;
+    //public float moveT;
+
+    //private void Start()
+    //{
+    //    StartCoroutine(MoveCor());
+    //}
+    //IEnumerator MoveCor()
+    //{
+    //    transform.position = GameManager.instance.IndexToPos(playerPos.x, playerPos.y);
+    //    while (true)
+    //    {
+    //        do
+    //        {
+
+    //            for (int i = 0; i < moveKey.Length; i++)
+    //            {
+    //                if (Input.GetKey(moveKey[i]))
+    //                {
+    //                    if (!inputDirList.Contains(dirVec[i]))
+    //                        inputDirList.Add(dirVec[i]);
+    //                }
+    //                else if (inputDirList.Contains(dirVec[i]))
+    //                {
+    //                    inputDirList.Remove(dirVec[i]);
+    //                }
+    //                yield return null;
+    //            }
+    //        }
+    //        while (inputDirList.Count == 0);
+
+    //        if (inputDirList[0] != Vector2Int.zero)
+    //        {
+    //            Vector2Int firstPos = GameManager.instance.ClampedIndex(playerPos + inputDirList[inputDirList.Count - 1]);
+    //            Vector2Int secondPos = GameManager.instance.ClampedIndex(playerPos + inputDirList[inputDirList.Count - 1] * 2);
+
+    //            PixelState firstState = GameManager.instance.GetPixel(firstPos);
+    //            PixelState secondState = GameManager.instance.GetPixel(secondPos);
+
+    //            bool check = secondState != PixelState.moveLine && secondState != PixelState.fill && firstState != PixelState.moveLine && firstState != PixelState.fill;
+
+    //            if (check)
+    //            {
+    //                if(GameManager.instance.GetPixel(playerPos)==PixelState.none)
+    //                    Ga
+    //            }
+    //        }
+    //        Vector3 pos = GameManager.instance.IndexToPos(playerPos.x, playerPos.y);
+    //        Vector3 orzPos = transform.position;
+    //        float per = 1 / moveT;
+    //        for (float t = 0; t < moveT; t += Time.deltaTime)
+    //        {
+    //            transform.position = Vector3.Lerp(orzPos, pos, t * per);
+    //            yield return null;
+    //        }
+    //        transform.position = pos;
+    //    }
+    //}
+    KeyCode[] moveKey = new KeyCode[4] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+    Vector2Int[] vecDir = new Vector2Int[4] { Vector2Int.up, Vector2Int.left, Vector2Int.down, Vector2Int.right };
+    List<Vector2Int> inputDirList = new();
+
+    public List<Vector2Int> moveLineList = new();
+
+    public float moveInterval;
+
     public Vector2Int playerPos;
-    public float moveT;
+    public Vector2Int PlayerPos
+    {
+        get => playerPos;
+        set
+        {
+            value = GameManager.instance.ClampedIndex(value);
+            playerPos = value;
+        }
+    }
 
     private void Start()
     {
-        StartCoroutine(MoveCor());
+        StartCoroutine(Move_Cor());
     }
-    IEnumerator MoveCor()
+    IEnumerator Move_Cor()
     {
         while (true)
         {
-            for (int i = 0; i < keyArr.Length; i++)
+            do
             {
-                if (Input.GetKey(keyArr[i]))
+                for (int i = 0; i < moveKey.Length; i++)
                 {
-                    playerPos += dir[i];
+                    if (Input.GetKey(moveKey[i]))
+                    {
+                        if (!inputDirList.Contains(vecDir[i]))
+                            inputDirList.Add(vecDir[i]);
+                    }
+                    else if (inputDirList.Contains(vecDir[i]))
+                    {
+                        inputDirList.Remove(vecDir[i]);
+                    }
+                    yield return null;
                 }
-                yield return null;
+            }
+            while (inputDirList.Count == 0);
+            if (inputDirList[0] != Vector2Int.zero)
+            {
+                PlayerPos += inputDirList[inputDirList.Count - 1];
+
+                if (PixelState.none == GameManager.instance.GetPixel(playerPos))
+                {
+                    moveLineList.Add(playerPos);
+                    GameManager.instance.SetPixel(playerPos, PixelState.moveLine);
+                }
             }
             Vector3 pos = GameManager.instance.IndexToPos(playerPos.x, playerPos.y);
             Vector3 orzPos = transform.position;
-            float per = 1 / moveT;
-            for (float t = 0; t<moveT;t+=Time.deltaTime)
+
+            float per = 1 / moveInterval;
+            for (float t = 0; t < moveInterval; t += Time.deltaTime)
             {
-                transform.position = Vector3.Lerp(orzPos, pos, t * per);
-                yield return null;
+                Vector3.Lerp(orzPos, pos, t * per);
             }
             transform.position = pos;
         }
